1148922fda95614c80456a7202764e3827f7f893
Michael DE FILIPPIS <michael.defilippis69005@gmail.com>
2023-06-12 17:11:16 +0200

    utilisation useEffect

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,19 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Button from './components/ui/Button/Button';
 
 function App() {
   const [counter, setcounter] = useState(0);
+  useEffect(()=> {
+    console.log('value post setCounter', counter)
+    return () => {
+      //console.log('cleanup')
+    }
+  } ,[counter])
+  useEffect(()=> {
+    console.log('creation du composant set etat initial')
+    setcounter(5)
+  }, [])
   return (
     <div className='App'>
       voici la valeur de counter : {counter}
