3e2e83152b98872e27ec6f09f61a3e1ea8784b9a
Michael DE FILIPPIS <michael.defilippis69005@gmail.com>
2023-06-12 17:42:09 +0200

    exercice changement class bouton lors du clic

--- src/App/components/ui/Button/Button.jsx
+++ src/App/components/ui/Button/Button.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, {useState , useEffect} from 'react'
 import style from './Button.module.css'
 import PropTypes from 'prop-types'
 
 const Button=(props)=>{
-    console.log(props)
+    const [isClicked, setIsClicked] = useState(false);
+    useEffect(()=> {
+        if(isClicked) {
+            setTimeout(()=>{
+                setIsClicked(false)
+            }, 4000)
+        }  
+        
+    }, [isClicked])
+    /*console.log(props)*/
     return (
     <button type={props.type}
             className={`${style.Button} btn${
                 undefined!==props.className ? ' ' + props.className : ''
-            }`}
+            }${true === isClicked ? ' '+ style.clicked : ''}`}
             onClick={(evt)=>{
-        if(undefined!==props.onClick && typeof props.onClick === 'function') {
-            props.onClick('click');
-        }
-    }}>
+                setIsClicked(true)
+                if(undefined!==props.onClick && typeof props.onClick === 'function') {
+                    props.onClick('click');
+                }
+                }
+            }>
         {props.children}
     </button>);
 }
