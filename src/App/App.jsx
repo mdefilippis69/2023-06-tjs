4dfdde56abc862c70ca014a368751530053731da
Michael DE FILIPPIS <michael.defilippis69005@gmail.com>
2023-06-12 16:50:42 +0200

    utilisation useState

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Button from './components/ui/Button/Button';
 
 function App() {
-  let counter=0;
+  const [counter, setcounter] = useState(0);
   return (
     <div className='App'>
       voici la valeur de counter : {counter}
       <hr/>
       <Button className={"primary"} onClick={()=> {
-        counter+=1
+        setcounter(counter+1)
         console.log(counter)
       }}>+1</Button>
       <Button className={"error"} onClick={()=>{
-        counter-=1
+        setcounter(counter-1)
         console.log(counter)
       }}>-1</Button>
     </div>
